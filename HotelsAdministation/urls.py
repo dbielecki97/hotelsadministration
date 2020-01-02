77891c6880384fce1a563ed2ebbd73af7a4191d0
Dawid Bielecki <dbielecki97@gmail.com>
2020-01-02 19:31:53 +0100

    pierwsze url i templaty dla home'ów

--- HotelsAdministation/urls.py
+++ HotelsAdministation/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reservation/', include('reservation.urls')),
+    path('worker/', include('worker.urls')),
+    path('catering/', include('catering.urls')),
+    path('servicerequest/', include('service_request.urls')),
+    path('hotel/', include('hotel.urls'))
 ]
